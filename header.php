b836b4279876c8d9e66749dd36129fe7095e2dfb
DeveloperSnehal <developer.creativemarque@gmail.com>
2024-01-02 14:46:34 +0530

    created new landing page

---
+++ header.php
@@ -0,0 +1,283 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Techwix - Technology IT Solutions Consultancy HTML5 Template</title>
+    <meta name="robots" content="noindex, follow" />
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
+
+    <!-- CSS
+	============================================ -->
+
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/all.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/flaticon.css">
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/aos.css">
+    <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+    <!--====== Use the minified version files listed below for better performance and remove the files listed above ======-->
+    <!-- <link rel="stylesheet" href="assets/css/vendor/plugins.min.css">
+    <link rel="stylesheet" href="assets/css/style.min.css"> -->
+
+</head>
+
+<body>
+
+    <div class="main-wrapper">
+
+
+        <!-- Preloader start -->
+        <div id="preloader">
+            <div class="preloader">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        <!-- Preloader End -->
+
+        <!-- Header Start  -->
+        <div id="header" class="section header-section header-section-06">
+
+            <!-- Header Top Start  -->
+            <div class="header-top-section d-none d-lg-block">
+                <div class="container">
+                    <div class="header-top-wrapper">
+                        <div class="header-top-info">
+                            <ul>
+                                <li><i class="fa fa-phone"></i> +36 55 540 069 </li>
+                                <li><i class="fa fa-envelope"></i> info.techwix@ourmail.com</li>
+                            </ul>
+                        </div>
+                        <div class="header-social">
+                            <span class="label">Follow Us</span>
+                            <ul>
+                                <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                                <li><a href="#"><i class="fab fa-twitter"></i> </a></li>
+                                <li><a href="#"><i class="fab fa-pinterest"></i> </a></li>
+                                <li><a href="#"><i class="fab fa-github"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Header Top End  -->
+
+            <div class="container">
+
+                <!-- Header Wrap Start  -->
+                <div class="header-wrap">
+
+                    <div class="header-logo">
+                        <a href="index.html"><img src="assets/images/logo.png" alt=""></a>
+                    </div>
+
+                    <div class="header-menu d-none d-lg-block">
+                        <ul class="main-menu">
+                            <li class="active-menu">
+                                <a href="index.html">Home</a>
+                                <ul class="sub-menu">
+                                    <li><a href="index.html">Home Main</a></li>
+                                    <li><a href="index-2.html">AI Solutions</a></li>
+                                    <li class="active"><a href="index-3.html">Cyber Security</a></li>
+                                    <li><a href="index-4.html">IT Solutions</a></li>
+                                    <li><a href="index-5.html">Software Company</a></li>
+                                    <li><a href="index-6.html">IT Agency</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="about.html">Aboute Us</a>
+                            </li>
+                            <li><a href="#">Pages</a>
+                                <ul class="sub-menu">
+                                    <li><a href="team.html">Our Team</a></li>
+                                    <li><a href="service.html">Service</a></li>
+                                    <li><a href="choose-us.html">Why Choose Us</a></li>
+                                    <li><a href="testimonial.html">Testimonial</a></li>
+                                    <li><a href="pricing.html">Pricing</a></li>
+                                    <li><a href="login-register.html">Login & Register</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Blog</a>
+                                <ul class="sub-menu">
+                                    <li><a href="blog.html">Blog Grid</a></li>
+                                    <li><a href="blog-standard.html">Blog List</a></li>
+                                    <li><a href="blog-details.html">Blog Single</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+
+                    <!-- Header Meta Start -->
+                    <div class="header-meta">
+                        <!-- Header Cart Start -->
+                        <div class="header-cart dropdown">
+                            <button class="cart-btn" data-bs-toggle="dropdown">
+                                <i class="flaticon-shopping-cart"></i>
+                                <span class="count">0</span>
+                            </button>
+                            <!-- Header Dropdown Cart Start -->
+                            <div class="dropdown-menu dropdown-cart">
+                                <!-- Dropdown Cart Items Start -->
+                                <div class="cart-items">
+                                    <!-- Single Cart Item Start -->
+                                    <div class="single-cart-item">
+                                        <div class="item-image">
+                                            <img src="assets/images/shop-cart-1.jpg" alt="cart">
+                                        </div>
+                                        <div class="item-content">
+                                            <h4 class="title"><a href="#">Apple Iphone X</a></h4>
+                                            <span class="quantity">2 x $59.99</span>
+                                        </div>
+                                        <button class="btn-close"></button>
+                                    </div>
+                                    <!-- Single Cart Item End -->
+                                    <!-- Single Cart Item Start -->
+                                    <div class="single-cart-item">
+                                        <div class="item-image">
+                                            <img src="assets/images/shop-cart-2.jpg" alt="cart">
+                                        </div>
+                                        <div class="item-content">
+                                            <h4 class="title"><a href="#">Sony Xperia Tablet</a></h4>
+                                            <span class="quantity">2 x $59.99</span>
+                                        </div>
+                                        <button class="btn-close"></button>
+                                    </div>
+                                    <!-- Single Cart Item End -->
+                                    <!-- Single Cart Item Start -->
+                                    <div class="single-cart-item">
+                                        <div class="item-image">
+                                            <img src="assets/images/shop-cart-3.jpg" alt="cart">
+                                        </div>
+                                        <div class="item-content">
+                                            <h4 class="title"><a href="#">Camera Digital</a></h4>
+                                            <span class="quantity">2 x $59.99</span>
+                                        </div>
+                                        <button class="btn-close"></button>
+                                    </div>
+                                    <!-- Single Cart Item End -->
+                                </div>
+                                <!-- Dropdown Cart Items End -->
+                                <!-- Dropdown Cart Total Start -->
+                                <div class="cart-total">
+                                    <span class="label">Subtotal:</span>
+                                    <span class="value">0</span>
+                                </div>
+                                <!-- Dropdown Cart Total End -->
+                                <!-- Dropdown Cart Button Start -->
+                                <div class="cart-btns">
+                                    <a class="btn" href="#">View Cart</a>
+                                    <a class="btn btn-2" href="#">Checkout</a>
+                                </div>
+                                <!-- Dropdown Cart Button End -->
+                            </div>
+                            <!-- Header Dropdown Cart End -->
+                        </div>
+                        <!-- Header Cart End -->
+                        <!-- Header Search Start -->
+                        <div class="header-search">
+                            <a class="search-btn" href="#"><i class="flaticon-loupe"></i></a>
+                            <div class="search-wrap">
+                                <div class="search-inner">
+                                    <i id="search-close" class="flaticon-close search-close"></i>
+                                    <div class="search-cell">
+                                        <form action="#">
+                                            <div class="search-field-holder">
+                                                <input class="main-search-input" type="search" placeholder="Search Your Keyword...">
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Header Search End -->
+
+                        <div class="header-btn d-none d-xl-block">
+                            <a class="btn btn-4" href="contact.html">Lets Talk</a>
+                        </div>
+                        <!-- Header Toggle Start -->
+                        <div class="header-toggle d-lg-none">
+                            <button data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </button>
+                        </div>
+                        <!-- Header Toggle End -->
+                    </div>
+                    <!-- Header Meta End  -->
+
+                </div>
+                <!-- Header Wrap End  -->
+
+            </div>
+        </div>
+        <!-- Header End -->
+
+        <!-- Offcanvas Start-->
+        <div class="offcanvas offcanvas-start" id="offcanvasExample">
+            <div class="offcanvas-header">
+                <!-- Offcanvas Logo Start -->
+                <div class="offcanvas-logo">
+                    <a href="index.html"><img src="assets/images/logo-white.png" alt=""></a>
+                </div>
+                <!-- Offcanvas Logo End -->
+                <button type="button" class="close-btn" data-bs-dismiss="offcanvas"><i class="flaticon-close"></i></button>
+            </div>
+
+            <!-- Offcanvas Body Start -->
+            <div class="offcanvas-body">
+                <div class="offcanvas-menu">
+                    <ul class="main-menu">
+                        <li class="active-menu">
+                            <a href="index.html">Home</a>
+                            <ul class="sub-menu">
+                                <li><a href="index.html">Home Main</a></li>
+                                <li><a href="index-2.html">AI Solutions</a></li>
+                                <li class="active"><a href="index-3.html">Cyber Security</a></li>
+                                <li><a href="index-4.html">IT Solutions</a></li>
+                                <li><a href="index-5.html">Software Company</a></li>
+                                <li><a href="index-6.html">IT Agency</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="about.html">Aboute Us</a>
+                        </li>
+                        <li><a href="#">Pages</a>
+                            <ul class="sub-menu">
+                                <li><a href="team.html">Our Team</a></li>
+                                <li><a href="service.html">Service</a></li>
+                                <li><a href="choose-us.html">Why Choose Us</a></li>
+                                <li><a href="testimonial.html">Testimonial</a></li>
+                                <li><a href="pricing.html">Pricing</a></li>
+                                <li><a href="login-register.html">Login & Register</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Blog</a>
+                            <ul class="sub-menu">
+                                <li><a href="blog.html">Blog Grid</a></li>
+                                <li><a href="blog-standard.html">Blog List</a></li>
+                                    <li><a href="blog-details.html">Blog Single</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Offcanvas Body End -->
+        </div>
+        <!-- Offcanvas End -->
