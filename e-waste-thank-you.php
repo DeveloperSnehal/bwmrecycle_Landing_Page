e527f5de372dd96e4461c58f86573b245f1f9953
DeveloperSnehal <developer.creativemarque@gmail.com>
2024-01-03 19:23:37 +0530

    added thank you page

---
+++ e-waste-thank-you.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
+
+    <!-- CSS
+	============================================ -->
+
+    <!-- Icon Font CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/all.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/flaticon.css">
+
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/swiper-bundle.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/aos.css">
+    <link rel="stylesheet" href="assets/css/plugins/magnific-popup.css">
+
+    <!-- Main Style CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+
+    <!--====== Use the minified version files listed below for better performance and remove the files listed above ======-->
+    <!-- <link rel="stylesheet" href="assets/css/vendor/plugins.min.css">
+    <link rel="stylesheet" href="assets/css/style.min.css"> -->
+
+</head>
+
+<body>
+
+    <div class="main-wrapper">
+        <!-- Preloader start -->
+        <div id="preloader">
+            <div class="preloader">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        <!-- Preloader End -->
+
+
+        <!-- Offcanvas Start-->
+        <div class="offcanvas offcanvas-start" id="offcanvasExample">
+            <div class="offcanvas-header">
+                <!-- Offcanvas Logo Start -->
+                <div class="offcanvas-logo">
+                    <a href="index.html"><img src="assets/images/logo-white.png" alt=""></a>
+                </div>
+                <!-- Offcanvas Logo End -->
+                <button type="button" class="close-btn" data-bs-dismiss="offcanvas"><i class="flaticon-close"></i></button>
+            </div>
+
+            <!-- Offcanvas Body Start -->
+            <div class="offcanvas-body">
+                <div class="offcanvas-menu">
+                    <ul class="main-menu">
+                        <li class="active-menu">
+                            <a href="index.html">Home</a>
+                            <ul class="sub-menu">
+                                <li><a href="index.html">Home Main</a></li>
+                                <li><a href="index-2.html">AI Solutions</a></li>
+                                <li><a href="index-3.html">Cyber Security</a></li>
+                                <li class="active"><a href="index-4.html">IT Solutions</a></li>
+                                <li><a href="index-5.html">Software Company</a></li>
+                                <li><a href="index-6.html">IT Agency</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="about.html">Aboute Us</a>
+                        </li>
+                        <li><a href="#">Pages</a>
+                            <ul class="sub-menu">
+                                <li><a href="team.html">Our Team</a></li>
+                                <li><a href="service.html">Service</a></li>
+                                <li><a href="choose-us.html">Why Choose Us</a></li>
+                                <li><a href="testimonial.html">Testimonial</a></li>
+                                <li><a href="pricing.html">Pricing</a></li>
+                                <li><a href="login-register.html">Login & Register</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Blog</a>
+                            <ul class="sub-menu">
+                                <li><a href="blog.html">Blog Grid</a></li>
+                                <li><a href="blog-standard.html">Blog List</a></li>
+                                    <li><a href="blog-details.html">Blog Single</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- Offcanvas Body End -->
+        </div>
+        <!-- Offcanvas End -->
+
+
+        <!-- Hero Start -->
+        <div class="section techwix-hero-section-04 d-flex align-items-center" style="background-image: url(assets/images/thank-you-bg-bwm-recycle-india-e-waste.jpg);">
+            <img class="shape-1" src="assets/images/shape/hero4-shape1.png" alt="">
+            <img class="shape-2" src="assets/images/shape/hero4-shape2.png" alt="">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-12">
+                        <!-- Hero Content Start -->
+                        <div class="hero-content text-center">
+                            <h3 class="sub-title" data-aos-delay="600" data-aos="fade-up">WE PROVIDE SUSTAINABLE RECYCLING SOLUTIONS</h3>
+                            <h2 class="title" data-aos="fade-up" data-aos-delay="800">Thank You</h2>
+                            <p data-aos="fade-up" data-aos-delay="900">We will contact you shortly and will help you with a tailored e-waste recycling plan for your organization.</p>
+                            <div class="hero-btn" data-aos="fade-up" data-aos-delay="1000">
+                                <a class="btn" href="http://bwmrecycle.com/">Back to Website</a>
+                            </div>
+                        </div>
+                        <!-- Hero Content End -->
+                    </div>
+                </div>
+            </div>
+            <div class="shape-svg">
+                <svg id="svg" viewbox="0, 0, 400,27.708333333333336">
+                    <g id="svgg">
+                        <path id="path0" d="M0.000 13.854 L 0.000 27.708 200.000 27.708 L 400.000 27.708 400.000 14.216 L 400.000 0.725 395.573 1.822 C 322.012 20.064,241.242 29.243,178.709 26.467 C 123.876 24.033,57.451 14.315,4.123 0.926 C 2.094 0.417,0.336 0.000,0.217 0.000 C 0.069 0.000,0.000 4.399,0.000 13.854"></path>
+                    </g>
+                </svg>
+            </div>
+        </div>
+        <!-- Hero End -->
+
+
+    <!-- JS
+    ============================================ -->
+    <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="assets/js/vendor/modernizr-3.11.2.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="assets/js/plugins/popper.min.js"></script>
+    <script src="assets/js/plugins/bootstrap.min.js"></script>
+
+    <!-- Plugins JS -->
+    <script src="assets/js/plugins/swiper-bundle.min.js"></script>
+    <script src="assets/js/plugins/aos.js"></script>
+    <script src="assets/js/plugins/waypoints.min.js"></script>
+    <script src="assets/js/plugins/back-to-top.js"></script>
+    <script src="assets/js/plugins/jquery.counterup.min.js"></script>
+    <script src="assets/js/plugins/appear.min.js"></script>
+    <script src="assets/js/plugins/jquery.magnific-popup.min.js"></script>
+
+
+    <!--====== Use the minified version files listed below for better performance and remove the files listed above ======-->
+
+
+    <!-- Main JS -->
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
